d2c5e5a61581a45d28ff61c1e161db77a2ff1104
Fco. Javier González Campos <efja.gc@gmail.com>
2021-09-11 19:43:42 +0200

    Test básico para probas

---
+++ jestSetupEnv.js
@@ -0,0 +1,7 @@
+process.env.APP_NAME = "ProxectOS API REST";
+process.env.APP_DESCRIPTION = "ProxectOS API REST";
+
+process.env.APP_HOST = "http://localhost";
+process.env.APP_PORT = "3000";
+
+process.env.API_PREFIX = "v";
